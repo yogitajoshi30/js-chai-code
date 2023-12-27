7bb0e3d430c19571f89b53b5440a42fac320cab5
yogitajoshi30 <124453287+yogitajoshi30@users.noreply.github.com>
2023-12-27 16:58:33 +0000

    data types done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // this is possible in browser, but not in nodejs
+
+console.log(3 + 3); // code readability should be high, the code should be written in a way so that anyone can understand.
+
+console.log("Yogita");
+
+let name = "yogita";
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+//DATA TYPES:
+// number => 2 to power 53
+// bigint=> when the number is very big then bigint is used
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => when the variable is created but the value isn't defined
+// symbol => used for uniqueness
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
