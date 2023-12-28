edb0509f5ffdc6d92512f8c607f9d45c6c2b1dc7
yogitajoshi30 <124453287+yogitajoshi30@users.noreply.github.com>
2023-12-28 14:14:22 +0000

    comparison done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1); => true(2 is converted into number and then compared with 1)
+// console.log("02" > 1);
+
+/*console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);*/
+
+// == (this operator automatically converts the data type if the data type is not.)
+// === (this operator strictly compares, if the data type on both sides is not same, then it returns false.)
+
+console.log("2" === 2);
