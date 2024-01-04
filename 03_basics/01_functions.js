b94b4b684bddc0f9148df2b73c844f484eb2c668
yogitajoshi30 <124453287+yogitajoshi30@users.noreply.github.com>
2024-01-04 15:57:25 +0000

    functions understood in detail

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -64,34 +64,58 @@ function loginUserMessage(username = "yogi") {
 //console.log(loginUserMessage()); => yogi just logged in, ye output isliye mila because ab hmne function k parameter ko default value de di hai jo ki tb chlegi jb hm use koi argument pass nahi kra re honge
 //console.log(loginUserMessage("vanshika")); => vanshika just logged in, yha mne argument pass kr diya to default value ni chli
 
-/*function calculateCartPrice(val1, val2, ...num1) {
+/*function calculateCartPrice(num1) {
+  return num1;
+}*/
+//agr hm koi e-commerce website build krte hai to usmein hmein pta nahi hota hai ki user kitne number of items add krne vala hai, to aise mein hmein sare items ka price add krna hota hai to jb bhi hmein ye nahi pta hoga ki kitne number of arguments aane vale hai to aise mein hmein apne parameters ready krne hote hai,
+
+//console.log(calculateCartPrice(200)); => 200, yha hmne single argument pass kra or parameter bhi single hi tha to ye output aaya
+//console.log(calculateCartPrice(200, 400, 500, 2000)); => 200, yha hmne multiple argument pass kre, but parameter single tha to first argument hi print hua
+
+/*function calculateCartPrice(...num1) {
+  //(...=> rest operator)
+  return num1;
+}*/
+
+//console.log(calculateCartPrice(200, 400, 500, 2000)); => [ 200, 400, 500, 2000 ], ab sare arguments ek array ke andar aa gye, because upar hmne num1 pe rest operator lga diya, jo ki multiple arguments ko bind krke ek array mein hmein de deta hai. Rest operator or spread operator ko same way se likha jata hai, i.e., ... , bs iske usecase pe depend krta hai ki kb ise rest bolna hai or kb spread
+
+function calculateCartPrice(val1, val2, ...num1) {
   return num1;
 }
 
-// console.log(calculateCartPrice(200, 400, 500, 2000))
+//console.log(calculateCartPrice(200, 400, 500, 2000)); => [ 500, 2000 ] , yha pe upar hmne parameter mein val1, or val2 di to 200 val1 mein chla gya or 400 val2 mein, baki k bache hue arguments num1 mein chle gye
 
-const user = {
-  username: "hitesh",
-  prices: 199,
-};
+/*const user = {
+  username: "Yogita",
+  age: 22,
+};*/
 
-function handleObject(anyobject) {
+/*function handleObject(anyobject) {
+  //function k parameter mein hmne anyobject likha taki hm usmein koi bhi object pass kr ske
   console.log(
-    `Username is ${anyobject.username} and price is ${anyobject.price}`
+    `Username is ${anyobject.username} and age is ${anyobject.age}` // => Username is Yogita and age is 22
   );
-}
+}*/
 
-// handleObject(user)
-handleObject({
+//handleObject(user); => function ko call krte time hmne usmein us object ka name pass kraya jismein se hm values lena chahte hai
+
+/*function handleObject(anyobject) {
+  console.log(
+    `Username is ${anyobject.username} and age is ${anyobject.age}` // => Username is sam and age is 38
+  );
+}*/
+
+/*handleObject({
   username: "sam",
-  price: 399,
-});
+  age: 38,
+});*/ // yha pe hmne object specially na bnake function ko call krte time hi bna diya hai,
 
+//Array bhi ek dm object ki hi trh hai
 const myNewArray = [200, 400, 100, 600];
 
 function returnSecondValue(getArray) {
   return getArray[1];
 }
 
-// console.log(returnSecondValue(myNewArray));
-console.log(returnSecondValue([200, 400, 500, 1000]));*/
+//console.log(returnSecondValue(myNewArray)); => 400
+console.log(returnSecondValue([200, 400, 500, 1000])); // => 400
