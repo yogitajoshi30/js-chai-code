34ff78afc2e6724a1ddf2dd639d1ed396f498fa3
Yogita Joshi <124453287+yogitajoshi30@users.noreply.github.com>
2024-03-29 18:24:20 +0000

    ADVANCED ARRAY

---
+++ 11_fun_with_js/02_array_advanced.js
@@ -0,0 +1,49 @@
+const myArr = [];
+%DebugPrint(myArr);
+
+// continuous, Holey
+
+// SMI (small integer)
+// Packed element
+// Double (float, sting, function)
+
+const arrTwo = [1, 2, 3, 4, 5];
+//PACKED_SMI_ELEMENTS
+
+arrTwo.push(6.0);
+//PACKED_DOUBLE_ELEMENTS
+
+arrTwo.push("7");
+//PACKED_ELEMENTS
+
+arrTwo[10] = 11;
+//HOLEY_ELEMENTS
+
+console.log(arrTwo); //=> [1, 2, 3, 4, 5, 6, '7', <3 empty items>, 11]
+console.log(arrTwo.length); //=> 11
+console.log(arrTwo[9]); //=> undefined
+
+//bound check
+//hasOwnProperty(arrTwo, 9);
+//hasOwnProperty(arrTwo.prototype, 10);
+//hasOwnProperty(Object.prototype, 10);
+
+//holes are very expensive in js
+
+const arrThree = [1, 2, 3, 4, 5];
+console.log(arrThree[2]);
+
+//SMI > DOUBLE > PACKED
+//H_SMI > H_DOUBLE > H_PACKED
+
+const arrFour = new Array(3);
+//just 3 holes. HOLEY_SMI_ELEMENTS
+arrFour[0] = "1"; //HOLEY_ELEMENTS
+arrFour[1] = "2"; //HOLEY_ELEMENTS
+arrFour[2] = "3"; //HOLEY_ELEMENTS
+
+//CORRECT WAY FOR BETTER OPTIMISATION
+const arrFive = [];
+arrFive[0] = "1"; //PACKED_ELEMENTS
+arrFive[1] = "2"; //PACKED_ELEMENTS
+arrFive[2] = "3"; //PACKED_ELEMENTS
